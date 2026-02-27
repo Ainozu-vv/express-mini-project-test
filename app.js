ac2537ad7dc3a1925183bbd4da18897e857b06a1
Ainozu-vv <simon.martin@vasvill.hu>
2026-02-27 08:15:47 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { router: itemsRouter } = require('./routes/items');
 const { router: categoriesRouter } = require('./routes/categories');
+const { router: carsRouter } = require('./routes/cars');
 
 const app = express();
 
 app.use(express.json());
 app.use('/items', itemsRouter);
 app.use('/categories', categoriesRouter);
+app.use('/cars', carsRouter);
 
 // Az app-ot exportáljuk, hogy a tesztek Supertest-tel tudják használni
 module.exports = app;
